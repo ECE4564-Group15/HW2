aae4fa654dc9d2cca57646d885784df83462b4c8
Beichen <lbc0430@gmail.com>
2017-02-23 20:48:12 -0500

    add gpio

--- pistatsview.py
+++ pistatsview.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python3
 import time
 import json
-import pymongo
+import os
+# 	import pymongo
 
 stats = '{ "net": {"lo": {"rx": 0,"tx": 0},"wlan0": {"rx": 708,"tx": 1192},"eth0": {"rx": 0,"tx":0}},"cpu": 0.2771314211797171}'
 
 data = json.loads(stats)
 
-def Print(data):
-    while True:
-        print("Host_1: ");
-        print("cpu: ", data['cpu']);
-        print("lo: rx=",data['net']['lo']['rx'])
-        time.sleep(1)
+def Print(host,data):
+    High = 1
+    Low = 0
+    print(host);
+    print("cpu: ", data['cpu']);
+    print("lo: rx=",data['net']['lo']['rx'],"[Hi: ", High, " B/s, Lo: ", \
+           Low,  " B/s], tx= ", data['net']['lo']['tx']," B/s [Hi: ", High, " B/s, Lo: ", \
+           Low,  " B/s]")
+    print("eth0: rx=",data['net']['eth0']['rx'],"[Hi: ", High, " B/s, Lo: ", \
+           Low,  " B/s], tx= ", data['net']['eth0']['tx']," B/s [Hi: ", High, " B/s, Lo: ", \
+           Low,  " B/s]")
+    print("wlan0: rx=",data['net']['wlan0']['rx'],"[Hi: ", High, " B/s, Lo: ", \
+           Low,  " B/s], tx= ", data['net']['wlan0']['tx']," B/s [Hi: ", High, " B/s, Lo: ", \
+           Low,  " B/s]")
+    time.sleep(1)
 
+def LED(cpuU) :
+   echo 0 >/sys/class/gpio/gpio17/value
+   echo 0 >/sys/class/gpio/gpio19/value
+   echo 0 >/sys/class/gpio/gpio21/value
+   if cpuU <0.25:
+       echo 1 >/sys/class/gpio/gpio17/value
+   elif cpuU >= 0.25 and cpuU < 0.5:
+       echo 1 >/sys/class/gpio/gpio19/value
+   else : 
+       echo 1 >/sys/class/gpio/gpio21/value
+
+       
 def main():
-    Print(data)
+    os.system("echo 17 > /sys/class/gpio/export") # blue
+    os.system("echo 19 > /sys/class/gpio/export") # green
+    os.system("echo 21 > /sys/class/gpio/export") # red
+    os.system("echo out > /sys/class/gpio/gpio17/direction")
+    os.system("echo out > /sys/class/gpio/gpio19/direction")
+    os.system("echo out > /sys/class/gpio/gpio21/direction")
+    Print("Host_1", data)
+    LED(data['cpu'])
+    
 
 main()
