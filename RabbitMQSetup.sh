6b201b4453c7270ac3d2028f5d531cca5fbaef59
Steven Frederiksen <sfrederiksen@vt.edu>
2017-02-26 19:38:15 -0500

    Added guest and root vhost support in the setup script.

--- RabbitMQSetup.sh
+++ RabbitMQSetup.sh
@@ -2,3 +2,6 @@
 sudo rabbitmqctl add_user Usage team15
 sudo rabbitmqctl add_vhost usage_vhost
 sudo rabbitmqctl set_permissions -p usage_vhost "Usage" ".*" ".*" ".*"
+sudo rabbitmqctl set_permissions -p usage_vhost "guest" ".*" ".*" ".*"
+sudo rabbitmqctl set_permissions -p / "Usage" ".*" ".*" ".*"
+sudo rabbitmqctl set_permissions -p / "guest" ".*" ".*" ".*"
